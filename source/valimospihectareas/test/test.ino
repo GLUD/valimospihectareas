51497d772bebeb8c299b865a9ffedb8acb21002f
kellar1896 <cgbarretor@correo.udistrital.edu.co>
2018-09-01 14:10:48 -0500

    se corrigen los errores de pines

--- source/valimospihectareas/test/test.ino
+++ source/valimospihectareas/test/test.ino
@@ -1,19 +1,19 @@
 char unsigned tablero[77]={
-  0,0,1,0,0,0,0,
-  0,0,0,1,0,1,0,
+  0,0,0,0,0,0,0,
+  0,0,0,0,0,0,0,
+  0,0,0,1,0,0,0,
+  0,0,0,0,1,0,0,
+  0,0,0,0,0,0,0,
+  0,0,0,1,1,0,0,
   1,0,0,0,0,0,0,
-  0,1,0,0,0,0,1,
-  0,0,0,0,0,1,1,
-  0,0,1,0,0,0,0,
-  1,0,0,0,0,0,1,
-  0,1,0,1,0,0,0,
-  0,0,1,0,1,1,1,
+  0,0,0,0,0,0,0,
+  0,0,0,0,0,0,0,
   0,0,0,0,0,0,0,
   0,0,0,0,0,0,0   /// esta fila son lo valores de desborde los cualesmseran evaluados para contar la vida a restar
   }; //inicio la matriz del tablero como un vector de 75 posiciones
  char unsigned  vida = 10; 
  int columnas[]={22, 23, 24, 25, 26, 27, 28};
- int filas[]={29, 30, 31, 32, 33, 34, 35, 36, 37, 38}; 
+ int filas[]={42, 43, 44, 45, 46, 47, 48, 49, 50, 51}; 
  char unsigned tableroM[7][10];
 
 
@@ -25,12 +25,15 @@ void setup() {
   for(int i=0; i<=9; i++){
     pinMode(filas[i], OUTPUT);
     }
+    VectoraMatriz();
   
   
 
 }
 
 void loop() {
+  
+  visu();
   
   }  
 
@@ -57,12 +60,12 @@ void evaluarmatriz(int i, int k){
       offF();
       digitalWrite(columnas[i], LOW);
       digitalWrite(filas[k], HIGH);
-      delay(10);
+      delay(1);
       } 
     else{
       offC();
       offF();
-      delay(10);
+      //delay(10);
       }  
   }   
 
@@ -78,7 +81,7 @@ void offC(){
 
 void offF(){
   for(int i =0; i<10 ; i++){
-    digitalWrite(columnas[i], LOW);
+    digitalWrite(filas[i], LOW);
     }
   }      
 
